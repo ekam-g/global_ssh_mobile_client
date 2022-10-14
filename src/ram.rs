744ce67589eac33a165dc97a9a60d12c1ec59c1c
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-14 13:06:45 -0400

    readme

--- src/ram.rs
+++ src/ram.rs
@@ -2,11 +2,11 @@ use std::sync::Mutex;
 use once_cell::sync::Lazy;
 
 pub struct SafeGlobalVar {
-    pub key_pressed_cashe: Vec<String>,
+    pub key_pressed_cache: Vec<String>,
 }
 
 pub static SAFE_PUB_VAR: Lazy<Mutex<SafeGlobalVar>> = Lazy::new(|| {
     Mutex::new(SafeGlobalVar {
-        key_pressed_cashe: "".to_ownd(),
+        key_pressed_cache: vec!["".to_owned()],
     })
 });
