c423b354327a6452ec1822b55ec0590a74d7ee74
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-14 13:30:01 -0400

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-mod ram;
-
 use std::{thread, time};
+
 use device_query::{DeviceQuery, DeviceState, Keycode};
+
+use crate::ram::SAFE_PUB_VAR;
+
+mod ram;
 
 fn main() {
     loop {
         let device_state = DeviceState::new();
         let keys: Vec<Keycode> = device_state.get_keys();
         for key_pressed in keys {
-            println!("Is pressed {}"  , key_pressed);
+            SAFE_PUB_VAR.lock().expect("todo").key_pressed_cache.push(key_pressed.to_string());
+            if key_pressed.to_string().contains('Q') {
+                SAFE_PUB_VAR.lock().expect("todo").key_pressed_cache = vec!["".to_string()];
+            }
+            for cache in SAFE_PUB_VAR.lock().expect("todo").key_pressed_cache.clone() {
+                println!("Is pressed {}", cache);
+            }
         }
         thread::sleep(time::Duration::from_millis(10))
     }
