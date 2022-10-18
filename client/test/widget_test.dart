e0700ba9ec1514e3ed90348f68cc3e41811a6d40
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-17 23:54:18 -0400

    error fix

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:client/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(sign_in: true,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
