dc2aecf53d9c865c5a10c777fb561f5938072591
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-19 22:38:56 -0400

    fix form

--- client/lib/screens/what_db.dart
+++ client/lib/screens/what_db.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key,});
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  TextEditingController redis1 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Client Keylogger Viewer"),
+        title: const Text("Please Sign In Your Redis Server"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: <Widget>[
+            TextField(
+              controller: redis1,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a search term',
+              ),
             ),
           ],
         ),
