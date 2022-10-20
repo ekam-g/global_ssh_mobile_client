6a4b33aa669ea3fcbf36c2b678f4024fa10bdf6c
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-20 15:12:38 -0400

    ui done for first screen

--- client/lib/screens/what_db.dart
+++ client/lib/screens/what_db.dart
@@ -1,3 +1,4 @@
+import 'package:client/widgets/coolButtion.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -11,6 +12,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController redis1 = TextEditingController();
+  TextEditingController redis2 = TextEditingController();
+  TextEditingController redis3 = TextEditingController();
+  TextEditingController redis4 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +23,78 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text("Please Sign In Your Redis Server"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: redis1,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter a search term',
+        child: SizedBox(
+          width: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Spacer(),
+              const Expanded(
+                flex: 5,
+                child: Image(
+                  image: AssetImage("lib/assets/hecker-removebg-preview.png"),
+                ),
               ),
-            ),
-          ],
+              Expanded(
+                flex: 1,
+                child: TextField(
+                  controller: redis1,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Redis Key',
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                flex: 1,
+                child: TextField(
+                  controller: redis2,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Redis Key',
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                flex: 1,
+                child: TextField(
+                  controller: redis3,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Redis Key',
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                flex: 1,
+                child: TextField(
+                  controller: redis4,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Redis Key',
+                  ),
+                ),
+              ),
+              const Spacer(),
+
+              ExpandedButton(
+                  onPressed: () {
+                    debugPrint(redis1.text);
+                    debugPrint(redis2.text);
+                    debugPrint(redis3.text);
+                    debugPrint(redis4.text);
+
+                  },
+                  text: "Set",
+                  flex: 2,
+                  fontSize: 12,
+                  width: 200),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
